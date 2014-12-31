000717b83ff4bdf9c30c1adc56bfda92f189af23
Tyler <chikinlad@msn.com>
2014-12-31 10:35:50 -0700

    change url to start at daily

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -8,7 +8,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns('', 
+	(r'^$', daily),
 	url(r'^hello/$', hello),
 	url(r'^time/$', current_datetime),
 	url(r'^time/plus/(\d{1,2})/$', hours_ahead),
