78e026d687bb9b3a755fe34637f74601851804d1
Tyler <chikinlad@msn.com>
2015-01-10 15:48:20 -0700

    nothing works :(

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 TEMPLATE_DIRS = (
@@ -71,6 +73,9 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': dj_database_url.config()
+}
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
