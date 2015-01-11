bc64583bf36782ae6039527601d4bcfc215f5b24
Tyler <chikinlad@msn.com>
2015-01-10 17:21:21 -0700

    nothing works :(

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -92,8 +92,8 @@ AUTH_PROFILE_MODULE = 'bible.UserProfile'
 LOGIN_REDIRECT_URL = '/daily/'
 
 # Parse database configuration from $DATABASE_URL
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
